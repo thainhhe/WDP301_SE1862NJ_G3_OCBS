81a4543fbfa5a43c6ba5c9371f86e435e9c33d08
thainhhe <146052091+thainhhe@users.noreply.github.com>
2025-05-21 23:54:28 +0700

    update server.js to models initial

--- back-end/server.js
+++ back-end/server.js
@@ -7,11 +7,51 @@ import path from "path";
 import { fileURLToPath } from "url";
 import connectDB from "./config/db.js";
 
-// Load environment variables
+// Load env
 dotenv.config();
 
-// Connect to MongoDB
+// Connect DB
 connectDB();
+
+// Import models (side-effect to register schema)
+import "./models/bookingModel.js";
+import "./models/seatModel.js";
+import "./models/seatLayoutModel.js";
+import "./models/seatStatusModel.js";
+import "./models/branchModel.js";
+import "./models/userModel.js";
+import "./models/movieModel.js";
+import "./models/showtimeModel.js";
+import "./models/comboModel.js";
+import "./models/voucherModel.js";
+
+// Import to call .init()
+import Booking from "./models/bookingModel.js";
+import Seat from "./models/seatModel.js";
+import SeatLayout from "./models/seatLayoutModel.js";
+import SeatStatus from "./models/seatStatusModel.js";
+import Branch from "./models/branchModel.js";
+import User from "./models/userModel.js";
+import Movie from "./models/movieModel.js";
+import Showtime from "./models/showtimeModel.js";
+import Combo from "./models/comboModel.js";
+import Voucher from "./models/voucherModel.js";
+
+// Force create index/collection
+Promise.all([
+  Booking.init(),
+  Seat.init(),
+  SeatLayout.init(),
+  SeatStatus.init(),
+  Branch.init(),
+  User.init(),
+  Movie.init(),
+  Showtime.init(),
+  Combo.init(),
+  Voucher.init(),
+])
+  .then(() => console.log("✅ MongoDB models initialized"))
+  .catch((err) => console.error("❌ Model initialization failed:", err));
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
